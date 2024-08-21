c1757fee4b876c3483982880a4c76de03cb1f196
Efiservice <efiservice.servicos@gmail.com>
2024-08-21 17:11:55 -0300

    adding everything

---
+++ src/unit_1/App.java
@@ -0,0 +1,11 @@
+package unit_1;
+
+public class App {
+    public static void main(String[] args){
+
+        Student student1 = new Student("Davi", 1);
+        student1.setGrades(10, 2);
+        System.out.println(student1.getAverage());
+
+    }
+}
