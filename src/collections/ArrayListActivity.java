64b6e4974982c107c9cf61446b31fdb3fcbfc6c2
Davi Rodrigues <davirnascimentox@gmail.com>
2024-08-21 19:14:54 -0300

    compilation problem - arrayList

--- src/collections/ArrayListActivity.java
+++ src/collections/ArrayListActivity.java
@@ -1,52 +1,45 @@
 package collections;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListActivity {
-
+    
     static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // 1 Create ArrayList
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        int num;
+        // 2 Fill it in
+        fillIn(list);
 
-        System.out.println("Type 10 different numbers, piece of shit: ");
-        for(int i = 0; i < 10; i++){
-            try {
-                num = Integer.parseInt(scan.nextLine());
-            } catch (Exception e){
-                System.out.println("Type only numbers.");
-                break;
-            }
-            numbers.add(i, num);
-        }
+        // 3 Print the original one
+        System.out.println("Original List: " + list);
 
-        if(numbers.size() == 10){
-            System.out.println("Lista após preenchimento: " + numbers);
-            System.out.println("Lista após remover pares: " + removeEven(numbers));
+        // 4 Remove even numbers from the list
+        removeEven(list);
+
+        // 5 Print the resulting list
+        System.out.println("Without even numbers list: " + list);
+    }
+
+    public static void fillIn(ArrayList<Integer> list) {
+        System.out.println("List size: ");
+        int size = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < size; i++) {
+            list.add((int)(Math.random() * 50 + 1));
         }
     }
 
-    // public static ArrayList<Integer> removeEven(ArrayList<Integer> arrayL){
-    //     for(int i = 0; i < arrayL.size(); i++){
-    //         if(arrayL.get(i) % 2 == 0){
-    //             arrayL.remove(arrayL.get(i));
-    //             i--;
-    //         }
-    //     }
-    //     return arrayL;
-    // }
-
-    // public static ArrayList<Integer> removeEven(ArrayList<Integer> arrayL){
-    //     Iterator<Integer> iterator  = arrayL.iterator();
-
-    //     while(iterator.hasNext()) {
-    //         int num = iterator.next();
-    //         if(num % 2 == 0){
-    //             iterator.remove();
-    //         }
-    //     }
-    //     return arrayL;
-    // }
+    public static void removeEven(ArrayList<Integer> list) {
+        int i = 0;
+        while (i < list.size()) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
+            } else {
+                i++;
+            }
+        }
+    }
 }
--- src/collections/LinkedListActivity.java
+++ src/collections/LinkedListActivity.java
@@ -3,14 +3,26 @@ package collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/*
+ * 3 - Faça um programa que preencha um ArrayList 
+ * ou uma LinkedList com números inteiros, mostre 
+ * essa lista na tela, remova todos os pares da lista 
+ * e mostre novamente na tela.
+ */
+
 public class LinkedListActivity {
 
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args){
+        // Create the Linked
         LinkedList<Integer> list = new LinkedList<>();
+
+        // Fill in the list
         fillIn(list);
+
         System.out.println("Full filled list: " + list);
+
     }
 
     public static void fillIn(LinkedList<Integer> list){
@@ -20,4 +32,6 @@ public class LinkedListActivity {
             list.add(i, (int)(Math.random()*50+1));
         }
     }
+
+    
 }
