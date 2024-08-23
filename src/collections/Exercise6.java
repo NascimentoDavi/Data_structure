a1ad927e8d652291767ead6d9a058627ef383228
Davi Rodrigues <davirnascimentox@gmail.com>
2024-08-23 11:48:16 -0300

    creating stack

---
+++ src/collections/Exercise6.java
@@ -0,0 +1,11 @@
+package collections;
+
+// 6 - a) Implemente uma classe PilhaInteiro que vai armazenar uma pilha de inteiros com as operações acima
+
+// b) Faça um aplicativo que vai armazenar na pilha vários valores inteiros (o programa pára de armazenar quando o usuário digitar 0 – zero), mostrar o valor que está no topo da pilha, remover todos os elementos da pilha e, em seguida, mostrar todos os elementos removidos na tela.
+
+public class Exercise6 {
+    public static void main(String[] args){
+
+    }
+}
