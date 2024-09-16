cd6d02e5e94127df172e9f0fdc5da71d78e4751a
NascimentoDavi <155047001+NascimentoDavi@users.noreply.github.com>
2024-09-16 14:55:27 -0300

    starting studyng collections

---
+++ ReviewC1/ArrayList1.java
@@ -0,0 +1,12 @@
+package ReviewC1;
+
+import java.util.ArrayListActivity;
+import java.util.Scanner;
+
+public class ArrayListActivity {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args){ 
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+    }
+}
